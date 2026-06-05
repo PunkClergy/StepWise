944529e70a9392d1efb8cb6c280df2b7dc054211
PunkMonk <43631789+PunkClergy@users.noreply.github.com>
2026-06-05 14:35:35 +0800

    feat：代码暂存

--- utils/EN.js
+++ utils/EN.js
@@ -1,73 +1,345 @@
 // 儿童英语单词库
 export const wordList = [{
 		word: "cat",
-		meaning: "猫",
+		meaning: "n.猫；猫咪；猫科动物",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=cat&type=1",
 		img: "/static/kidsEnglish/cat.png"
 	},
 	{
 		word: "dog",
-		meaning: "狗",
+		meaning: "n.狗；小狗",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=dog&type=1",
 		img: "/static/kidsEnglish/dog.png"
 	},
 	{
 		word: "apple",
-		meaning: "苹果",
+		meaning: "n.苹果",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=apple&type=1",
 		img: "/static/kidsEnglish/apple.png"
 	},
 	{
 		word: "banana",
-		meaning: "香蕉",
+		meaning: "n.香蕉",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=banana&type=1",
 		img: "/static/kidsEnglish/banana.png"
 	},
 	{
 		word: "bird",
-		meaning: "小鸟",
+		meaning: "n.小鸟；鸟类",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=bird&type=1",
 		img: "/static/kidsEnglish/bird.png"
 	},
 	{
 		word: "egg",
-		meaning: "鸡蛋",
+		meaning: "n.鸡蛋",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=egg&type=1",
 		img: "/static/kidsEnglish/egg.png"
 	},
 	{
 		word: "robin",
-		meaning: "知更鸟",
+		meaning: "n.知更鸟",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=robin&type=1",
 		img: "/static/kidsEnglish/robin.png"
 	},
 	{
 		word: "peacock",
-		meaning: "孔雀",
+		meaning: "n.孔雀",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=peacock&type=1",
 		img: "/static/kidsEnglish/peacock.png"
 	},
 	{
 		word: "parrot",
-		meaning: "鹦鹉",
+		meaning: "n.鹦鹉",
 		date: "2026-06-02",
 		audio: "https://dict.youdao.com/dictvoice?audio=parrot&type=1",
 		img: "/static/kidsEnglish/parrot.png"
 	},
-	// 示例
+	// 5.27
 	{
-		word: "xxx",
-		meaning: "xx",
-		date: "2026-06-02",
-		audio: "https://dict.youdao.com/dictvoice?audio=parrot&type=1",
-		img: "/static/kidsEnglish/parrot.png"
+		word: "eggshell",
+		meaning: "n.蛋壳",
+		date: "2026-05-27",
+		audio: "https://dict.youdao.com/dictvoice?audio=eggshell&type=1",
+		img: "/static/kidsEnglish/eggshell.png"
 	},
+	{
+		word: "egg",
+		meaning: "n.鸡蛋",
+		date: "2026-05-27",
+		audio: "https://dict.youdao.com/dictvoice?audio=egg&type=1",
+		img: "/static/kidsEnglish/egg.png"
+	},
+	{
+		word: "barn",
+		meaning: "n.谷仓；牲口棚",
+		date: "2026-05-27",
+		audio: "https://dict.youdao.com/dictvoice?audio=barn&type=1",
+		img: "/static/kidsEnglish/barn.png"
+	},
+	//5.25
+	{
+		word: "fan",
+		meaning: "n.风扇；扇子",
+		date: "2026-05-25",
+		audio: "https://dict.youdao.com/dictvoice?audio=fan&type=1",
+		img: "/static/kidsEnglish/fan.png"
+	},
+	{
+		word: "pan",
+		meaning: "n.平底锅",
+		date: "2026-05-25",
+		audio: "https://dict.youdao.com/dictvoice?audio=pan&type=1",
+		img: "/static/kidsEnglish/pan.png"
+	},
+	{
+		word: "man",
+		meaning: "n.男人；成年人",
+		date: "2026-05-25",
+		audio: "https://dict.youdao.com/dictvoice?audio=man&type=1",
+		img: "/static/kidsEnglish/man.png"
+	},
+	{
+		word: "can",
+		meaning: "v.能够；n.罐头",
+		date: "2026-05-25",
+		audio: "https://dict.youdao.com/dictvoice?audio=can&type=1",
+		img: "/static/kidsEnglish/can.png"
+	},
+	{
+		word: "under",
+		meaning: "prep.在……下面",
+		date: "2026-05-25",
+		audio: "https://dict.youdao.com/dictvoice?audio=under&type=1",
+		img: "/static/kidsEnglish/under.png"
+	},
+	//5.20
+	{
+		word: "ram",
+		meaning: "n.公羊",
+		date: "2026-05-20",
+		audio: "https://dict.youdao.com/dictvoice?audio=ram&type=1",
+		img: "/static/kidsEnglish/ram.png"
+	},
+	{
+		word: "jam",
+		meaning: "n.果酱；堵塞",
+		date: "2026-05-20",
+		audio: "https://dict.youdao.com/dictvoice?audio=jam&type=1",
+		img: "/static/kidsEnglish/jam.png"
+	},
+	{
+		word: "yam",
+		meaning: "n.山药；甘薯",
+		date: "2026-05-20",
+		audio: "https://dict.youdao.com/dictvoice?audio=yam&type=1",
+		img: "/static/kidsEnglish/yam.png"
+	},
+	{
+		word: "dam",
+		meaning: "n.水坝",
+		date: "2026-05-20",
+		audio: "https://dict.youdao.com/dictvoice?audio=dam&type=1",
+		img: "/static/kidsEnglish/dam.png"
+	},
+	//5.11
+	{
+		word: "rabbit",
+		meaning: "n.兔子",
+		date: "2026-05-11",
+		audio: "https://dict.youdao.com/dictvoice?audio=rabbit&type=1",
+		img: "/static/kidsEnglish/rabbit.png"
+	},
+	{
+		word: "chicken",
+		meaning: "n.小鸡；鸡肉",
+		date: "2026-05-11",
+		audio: "https://dict.youdao.com/dictvoice?audio=chicken&type=1",
+		img: "/static/kidsEnglish/chicken.png"
+	},
+	{
+		word: "sheep",
+		meaning: "n.绵羊",
+		date: "2026-05-11",
+		audio: "https://dict.youdao.com/dictvoice?audio=sheep&type=1",
+		img: "/static/kidsEnglish/sheep.png"
+	},
+	{
+		word: "lamp",
+		meaning: "n.台灯；灯",
+		date: "2026-05-11",
+		audio: "https://dict.youdao.com/dictvoice?audio=lamp&type=1",
+		img: "/static/kidsEnglish/lamp.png"
+	},
+
+	//4.27
+	{
+		word: "seagull",
+		meaning: "n.海鸥",
+		date: "2026-04-27",
+		audio: "https://dict.youdao.com/dictvoice?audio=seagull&type=1",
+		img: "/static/kidsEnglish/seagull.png"
+	},
+	//4.23
+	{
+		word: "wax",
+		meaning: "n.蜡",
+		date: "2026-04-23",
+		audio: "https://dict.youdao.com/dictvoice?audio=wax&type=1",
+		img: "/static/kidsEnglish/wax.png"
+	},
+	{
+		word: "fox",
+		meaning: "n.狐狸",
+		date: "2026-04-23",
+		audio: "https://dict.youdao.com/dictvoice?audio=fox&type=1",
+		img: "/static/kidsEnglish/fox.png"
+	},
+	{
+		word: "box",
+		meaning: "n.盒子",
+		date: "2026-04-23",
+		audio: "https://dict.youdao.com/dictvoice?audio=box&type=1",
+		img: "/static/kidsEnglish/box.png"
+	},
+	{
+		word: "six",
+		meaning: "num.六",
+		date: "2026-04-23",
+		audio: "https://dict.youdao.com/dictvoice?audio=six&type=1",
+		img: "/static/kidsEnglish/six.png"
+	},
+	//4.14
+	{
+		word: "shorts",
+		meaning: "n.短裤",
+		date: "2026-04-14",
+		audio: "https://dict.youdao.com/dictvoice?audio=shorts&type=1",
+		img: "/static/kidsEnglish/shorts.png"
+	},
+	{
+		word: "hat",
+		meaning: "n.帽子",
+		date: "2026-04-14",
+		audio: "https://dict.youdao.com/dictvoice?audio=hat&type=1",
+		img: "/static/kidsEnglish/hat.png"
+	},
+	//4.8
+	{
+		word: "windy",
+		meaning: "adj.刮风的；多风的",
+		date: "2026-04-08",
+		audio: "https://dict.youdao.com/dictvoice?audio=windy&type=1",
+		img: "/static/kidsEnglish/windy.png"
+	},
+	{
+		word: "rainy",
+		meaning: "adj.下雨的；多雨的",
+		date: "2026-04-08",
+		audio: "https://dict.youdao.com/dictvoice?audio=rainy&type=1",
+		img: "/static/kidsEnglish/rainy.png"
+	},
+	{
+		word: "cold",
+		meaning: "adj.寒冷的",
+		date: "2026-04-08",
+		audio: "https://dict.youdao.com/dictvoice?audio=cold&type=1",
+		img: "/static/kidsEnglish/cold.png"
+	},
+	{
+		word: "hot",
+		meaning: "adj.炎热的",
+		date: "2026-04-08",
+		audio: "https://dict.youdao.com/dictvoice?audio=hot&type=1",
+		img: "/static/kidsEnglish/hot.png"
+	},
+	//4.1
+	{
+		word: "boots",
+		meaning: "n.靴子",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=boots&type=1",
+		img: "/static/kidsEnglish/boots.png"
+	},
+	{
+		word: "raincoat",
+		meaning: "n.雨衣",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=raincoat&type=1",
+		img: "/static/kidsEnglish/raincoat.png"
+	},
+	{
+		word: "umbrella",
+		meaning: "n.雨伞",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=umbrella&type=1",
+		img: "/static/kidsEnglish/umbrella.png"
+	},
+	{
+		word: "van",
+		meaning: "n.厢式货车",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=van&type=1",
+		img: "/static/kidsEnglish/van.png"
+	},
+	{
+		word: "vet",
+		meaning: "n.兽医",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=vet&type=1",
+		img: "/static/kidsEnglish/vet.png"
+	},
+	{
+		word: "vest",
+		meaning: "n.背心",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=vest&type=1",
+		img: "/static/kidsEnglish/vest.png"
+	},
+	{
+		word: "violin",
+		meaning: "n.小提琴",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=violin&type=1",
+		img: "/static/kidsEnglish/violin.png"
+	},
+	{
+		word: "jacket",
+		meaning: "n.夹克衫",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=jacket&type=1",
+		img: "/static/kidsEnglish/jacket.png"
+	},
+	{
+		word: "suit",
+		meaning: "n.西装",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=suit&type=1",
+		img: "/static/kidsEnglish/suit.png"
+	},
+	{
+		word: "bathing",
+		meaning: "v.洗澡",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=bathing&type=1",
+		img: "/static/kidsEnglish/bathing.png"
+	},
+	{
+		word: "sandals",
+		meaning: "n.凉鞋",
+		date: "2026-04-01",
+		audio: "https://dict.youdao.com/dictvoice?audio=sandals&type=1",
+		img: "/static/kidsEnglish/sandals.png"
+	}
 ];
+
+// 展示获客(联系我们页面)    谷歌推广   FB推广   谷歌统计代码(重点);   URL可定义;  服务器和CDN(可能无法支持) 页面响应速度(重点)  SEO和SEM词汇和停留监测
+// 支持SEO 谷歌SEO(重点)
+// UI效果(加分项)
